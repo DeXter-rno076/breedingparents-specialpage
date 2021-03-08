285f449dba3c2f1ed4e068bad6b1f71fa572459c
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2021-03-08 21:24:35 +0100

    added private attributes  and , replaced an unwanted variable in constructor call, corrected  name, added createSVG() call

--- includes/FrontendHandler.php
+++ includes/FrontendHandler.php
@@ -4,18 +4,22 @@ require 'SVGHandler.php';
 
 class FrontendHandler {
 	private $breedingTree = null;
+	private $pkmnData = null;
+	private $PKMN_ICON_HEIGHT = 50;
 	
-	public function __construct ($breedingTree) {
+	public function __construct ($breedingTree, $pkmnData) {
 		$this->breedingTree = $breedingTree;
+		$this->pkmnData = $pkmnData;
 	}
 
 	public function addSVG ($output) {
-		$preparator = new FrontendPreparator($this->breedingTree);
+		$preparator = new FrontendPreparator($this->pkmnData, $this->PKMN_ICON_HEIGHT);
 		$svgObjectStructure = $preparator->prepareForFrontend($this->breedingTree);
 
 		$maxDeepness = $svgObjectStructure->maxDeepness;
-		$generalHeight = $svgObjectStructure->height;
-		$svgHandler = new SVGHandler($this->breedingTree, $maxDeepness, $output, $generalHeight);
+		$generalHeight = $svgObjectStructure->generalHeight;
+		$svgHandler = new SVGHandler($svgObjectStructure, $maxDeepness, $generalHeight, $this->PKMN_ICON_HEIGHT);
+		$svgHandler->createSVG($output);
 	}
 }
 ?>
