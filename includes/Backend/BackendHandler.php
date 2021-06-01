9245be8ad6ec722522e46dd7567aed071f33b117
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2021-06-02 01:50:23 +0200

    removing separate function for updating egg groups blacklist (not really needed)

--- includes/Backend/BackendHandler.php
+++ includes/Backend/BackendHandler.php
@@ -10,7 +10,7 @@ class BackendHandler {
 	/**
 	 * main function that creates and returns the breeding tree
 	 */
-	public function createBreedingTree () : BreedingChainNode {
+	public function createBreedingTree (): BreedingChainNode {
 		//for performance measuring
 		$timeStart = hrtime(true);
 
@@ -19,8 +19,7 @@ class BackendHandler {
 		$targetPkmnData = Constants::$pkmnData->$targetPkmnName;
 
 		//needed for preventing infinite recursion
-		//a pkmn may only occur once in a branch,
-		//		otherwise you would get an infinite loop
+		//a pkmn may only occur once in a branch, otherwise you would get an infinite loop
 		$pkmnBlacklist = [$targetPkmnData->name];
 		$eggGroupBlacklist = [];
 
@@ -40,8 +39,7 @@ class BackendHandler {
 	/** 
 	 * depending on strictness pkmnBlacklist should use 
 	 * 		pass by reference (stricter, faster, inaccurate) or 
-	 * 		pass by value (looser,
-	 * 			slower (in extreme cases more then 200 times slower), more accurate)
+	 * 		pass by value (looser, slower (in extreme cases more then 200 times slower), more accurate)
 	 * 
 	 * returns a BreedingChainNode object if pkmn can learn/inherit the move
 	 * 		returns null if not
@@ -50,26 +48,21 @@ class BackendHandler {
 		StdClass $pkmnObj,
 		Array $eggGroupBlacklist,
 		Array $pkmnBlacklist
-	) : ?BreedingChainNode {
+	): ?BreedingChainNode {
 		//todo form change moves (e. g. Rotom) should count as normal 
 		//todo (check if there are breedable pkmn that have those ^ form change learnsets)
 		$node = new BreedingChainNode($pkmnObj->name);
 
 		if ($this->canLearnNormally($pkmnObj)) {
-			//if a pkmn can learn the targeted move directly without breeding
-			//		no possible successors are needed/wanted
+			//if a pkmn can learn the targeted move directly without breeding no possible successors are needed/wanted
 			//this happens at the end of a tree branch
-			//todo if targetPkmn is able to learn targetMove directly,
-			//todo		it looks like something went wrong
-			//todo		(only targetPkmn's icon is displayed)
+			//todo if targetPkmn is able to learn targetMove directly, it looks like something went wrong (only targetPkmn's icon is displayed)
 			return $node;
 		}
 
 		if ($this->canInherit($pkmnObj)) {
-			//calls createBreedingChainNode(...) for all suiting parents
-			//		(i. e. not in any blacklist)
-			//	and adds them as a successor to chainNode
-			//	if they can learn the move in some way
+			//calls createBreedingChainNode(...) for all suiting parents (i. e. not in any blacklist)
+			//	and adds them as a successor to chainNode if they can learn the move in some way
 			$eggGroup2 = null;
 			if (isset($pkmnObj->eggGroup2)) {
 				//this prevents masses of debugging messages
@@ -85,12 +78,10 @@ class BackendHandler {
 			);
 
 			//todo this explanation is not the yellow from the egg
-			//if a pkmn has no successors
-			//		that can learn the targeted move 
+			//if a pkmn has no successors that can learn the targeted move 
 			//		(with the corresponding blacklists for the branch)
 			//		it hasn't anyone to inherit the move from 
-			//		--> branch doesn't get added to existing tree structure
-			//		because there is no 'successful' end
+			//		--> branch doesn't get added to existing tree structure because there is no 'successful' end
 			if (count($node->getSuccessors()) > 0) {
 				//only return if the pkmn has at least one pkmn it can inherit the move from
 				return $node;
@@ -98,11 +89,9 @@ class BackendHandler {
 		}
 
 		if ($this->canLearnViaEvent($pkmnObj)) {
-			//event learnsets can be hard or impossible to get
-			//	so they are only checked when there is no other way
+			//event learnsets can be hard or impossible to get so they are only checked when there is no other way
 	
-			//marks that the chain node can only learn the move
-			//	via event learnsets (needed for frontend) 
+			//marks that the chain node can only learn the move via event learnsets (needed for frontend) 
 			$node->setLearnsByEvent();
 			return $node;
 		}
@@ -164,44 +153,20 @@ class BackendHandler {
 	) {
 		$eggGroupPkmnList = Constants::$eggGroups->$eggGroup;
 		$filter = new SuccessorFilter($node, $pkmnBlacklist, $eggGroupPkmnList);
-		$filteredList = $filter->filter();
+		$filteredList = $filter->filter();//the filter filters the unfiltered pkmn to get a filterd filter result
 
 		foreach ($filteredList as $potSuccessorName) {
 			$potSuccessorData = Constants::$pkmnData->$potSuccessorName;
 
-			$newEggGroupBlacklist = $this->createNewEggGroupBlacklist(
-				$eggGroup,
-				$otherEggGroup,
-				$eggGroupBlacklist
-			);
+			$eggGroupBlacklist[] = $eggGroup;
 
 			$this->addSuccessor(
 				$node,
 				$potSuccessorData,
-				$newEggGroupBlacklist,
+				$eggGroupBlacklist,
 				$pkmnBlacklist
 			);
 		}
-	}
-
-	private function createNewEggGroupBlacklist (
-		String $eggGroup,
-		?String $otherEggGroup,
-		Array $eggGroupBlacklist
-	) : Array {
-		/* if (!is_null($otherEggGroup)) {
-			$newEggGroupBlacklist = array_merge(
-				$newEggGroupBlacklist,
-				[$otherEggGroup]
-			);
-		} */
-		//* this handling of the blacklists is more inaccurate
-		//*		 but creates less large amounts of results
-		$newEggGroupBlacklist = array_merge(
-			$eggGroupBlacklist,
-			[$eggGroup]
-		);
-		return $newEggGroupBlacklist;
 	}
 
 	//todo name is meh (addSuccessor is already used in BreedingChainNode)
