14cbcd014d76e1fb2a3d2ec3e71c76f433c2ffc9
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2022-02-05 11:39:01 +0100

    changing comments and handling pot error in setting whitelisted egg group

--- includes/tree creation/SuccessorFilter.php
+++ includes/tree creation/SuccessorFilter.php
@@ -5,37 +5,37 @@ require_once __DIR__.'/../exceptions/AttributeNotFoundException.php';
 require_once 'PkmnData.php';
 
 class SuccessorFilter {
-    /*
-    has a function similar to a real world filter
-    takes a list of possible successors (i. e. an egg group list)
-    and removes every pkmn that isn't compatible to the situation
-        generally all pkmn that can't breed
-        pkmn that only have one gender aren't an option in some cases
-    */
     private Array $eggGroupBlacklist;
+
     /**
+     * @var String egg group whose pkmn are checked and added after this filter did its job
      * using a whitelist variable seems unnecessarily complex
      * but is necessary (at least i haven't found a better solution).
      * Just adding the egg group after filtering the successors would work
-     * for almost all nodes, BUT not for the root. there the first handled
+     * for almost all nodes, BUT not for the root. There the first handled
      * egg group could include the second egg group which would result in
      * redundancies or a wrong tree section.
      */
     private String $whitelistedEggGroup;
+
     private Array $successorList;
 
     public function __construct (
         Array $eggGroupBlacklist,
         String $whitelistedEggGroup,
-        Array $successorList
     ) {
         Logger::statusLog('creating SuccessorFilter instance with: '
             .'eggGroupBlacklist: '.json_encode($eggGroupBlacklist)
-            .'whiteListedEggGroup: '.json_encode($whitelistedEggGroup)
-            .'successorList: '.json_encode($successorList));
+            .'whiteListedEggGroup: '.json_encode($whitelistedEggGroup));
         $this->eggGroupBlacklist = $eggGroupBlacklist;
         $this->whitelistedEggGroup = $whitelistedEggGroup;
-        $this->successorList = $successorList;
+
+        if (isset(Constants::$eggGroups->$whitelistedEggGroup)) {
+            $this->successorList = Constants::$eggGroups->$whitelistedEggGroup;
+        } else {
+            $this->successorList = [];
+            Logger::elog('egg group '.$whitelistedEggGroup.' is not set');
+        }
     }
 
     public function filter (): Array {
@@ -83,6 +83,11 @@ class SuccessorFilter {
         $this->remove($pkmnIsBlacklisted);
     }
 
+    /**
+     * Removes all pkmn that are unpairable.
+     * This filter is supposed to only let those pkmn through that could be a suiting breeding parent.
+     * Pkmn that can't be paired can't possible be a parent.
+     */
     private function removeUnpairables () {
         Logger::statusLog('removing unpairable pkmn');
         /*unpairable pkmn cant get children
@@ -102,16 +107,19 @@ class SuccessorFilter {
         $this->remove($isUnpairable);
     }
 
+    /**
+     * Checks filters that only apply in some gens, like only male pkmn can pass on moves up to gen 5.
+     */
     private function checkGenSpecificRequirements () {
         Logger::statusLog('removing by gen specific requirements');
         if (Constants::$targetGen < 6) {
             Logger::statusLog('targetGen is < 6 => checking genders');
             //in gens 2-5 only fathers can give moves to their kids
-            $this->checkGender();
+            $this->removeFemaleOnlys();
         }
     }
 
-    private function checkGender () {
+    private function removeFemaleOnlys () {
         Logger::statusLog('removing only female pkmn');
         $isFemale = function (string $pkmn): bool {
             $pkmnData = null;
@@ -130,13 +138,20 @@ class SuccessorFilter {
         $this->remove($isFemale);
     }
 
+    /**
+     * Removes all successors for which $condition returns true.
+     * @param mixed $condition - function that returns a boolean and determines whether the given pkmn shall be removed
+     * 
+     */
     private function remove ($condition) {
-        for($i = 0; $i < count($this->successorList); $i++) {
+        for ($i = 0; $i < count($this->successorList); $i++) {
+
             $pkmn = $this->successorList[$i];
             if ($condition($pkmn)) {
                 array_splice($this->successorList, $i, 1);
                 $i--;
             }
+
         }
     }
 }
